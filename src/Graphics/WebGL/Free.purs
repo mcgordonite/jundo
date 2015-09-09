48bcff57721cd24a374e8cc7728af56c6d575019
Matthew Gordon <mc.gordonite@gmail.com>
2015-09-09 14:56:42 +0200

    Added free monad interpretation.

--- src/Graphics/WebGL/Free.purs
+++ src/Graphics/WebGL/Free.purs
@@ -7,7 +7,7 @@ import Prelude
 import Control.Monad.Eff
 import Control.Monad.Free
 import Graphics.Canvas (Canvas())
-import qualified Graphics.WebGL.Raw as GL
+import qualified Graphics.WebGL.Raw as R
 import Graphics.WebGL.Raw.Types
 
 data WebGLF a
@@ -26,6 +26,38 @@ instance functorWebGLF :: Functor WebGLF where
 
 type WebGL = Free WebGLF
 
---viewport :: forall eff. WebGLContext -> GLint -> GLint -> GLsizei -> GLsizei- -> Eff (canvas :: Canvas | eff) Unit 
---viewport :: GLint -> GLint -> GLsizei -> GLsizei -> WebGL Unit
---viewport = liftF $ Viewport 
+clear :: GLbitfield -> WebGL Unit
+clear mask = liftF $ Clear mask unit
+
+clearColor :: GLclampf -> GLclampf -> GLclampf -> GLclampf -> WebGL Unit
+clearColor r g b a = liftF $ ClearColor r g b a unit
+
+depthFunc :: GLenum -> WebGL Unit
+depthFunc func = liftF $ DepthFunc func unit
+
+enable :: GLenum -> WebGL Unit
+enable cap = liftF $ Enable cap unit
+
+viewport :: GLint -> GLint -> GLsizei -> GLsizei -> WebGL Unit
+viewport x y w h = liftF $ Viewport x y w h unit
+
+runWebGL :: forall a eff. R.WebGLContext -> WebGL a -> Eff (canvas :: Canvas | eff) a
+runWebGL gl = runFreeM interpret
+	where
+	interpret :: WebGLF (WebGL a) -> Eff (canvas :: Canvas | eff) a
+	interpret (Clear mask rest) = do
+		R.clear gl mask
+		return rest
+	interpret (ClearColor r g b a rest) = do
+		R.clearColor gl r g b a
+		return rest
+	interpret (DepthFunc func rest) = do
+		R.depthFunc gl func
+		return rest
+	interpret (Enable cap) = do
+		R.enable gl cap
+		return rest
+	interpret (Viewport x y w h rest) = do
+		R.viewport gl x y w h
+		return rest
+	
